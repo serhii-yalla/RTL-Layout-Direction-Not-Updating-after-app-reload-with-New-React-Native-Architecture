87ae575f482a730656b9dd3d297b83e5eac2be2b
Serhii Podoliuk <serhii@favecodes.com>
2024-07-26 10:45:23 +0300

    Add reproducer

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -5,113 +5,51 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useCallback} from 'react';
+import {Button, I18nManager, StyleSheet, Text, View} from 'react-native';
+import RNRestart from 'react-native-restart';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
+  const onApplyRTL = useCallback(() => {
+    if (!I18nManager.isRTL) {
+      I18nManager.forceRTL(true);
+      RNRestart.restart();
+    }
+  }, []);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const onApplyLTR = useCallback(() => {
+    if (I18nManager.isRTL) {
+      I18nManager.forceRTL(false);
+      RNRestart.restart();
+    }
+  }, []);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.area}>
+      <Text>Test Block</Text>
+      <View style={styles.testBlock}>
+        <Text>Left</Text>
+        <Text>Right</Text>
+      </View>
+      <View style={styles.testBlock}>
+        <Text>Left</Text>
+        <Text>Right</Text>
+      </View>
+      <Button title={'Apply RTL'} onPress={onApplyRTL} />
+      <Button title={'Apply LTR'} onPress={onApplyLTR} />
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
+  area: {
+    marginVertical: 50,
     paddingHorizontal: 24,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  testBlock: {
+    paddingVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
 
